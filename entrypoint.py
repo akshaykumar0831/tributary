bbfbae4d6e7f956a55643c6c863ef6ed96e2a43f
akshkham <akshaykumar.khambam@magna.com>
2024-11-22 20:09:05 -0500

    created flask endpoints and aggregated data from redis instance

--- entrypoint.py
+++ entrypoint.py
@@ -9,6 +9,7 @@ DATA_KEY = "engine_temperature"
 
 # create a Flask server, and allow us to interact with it using the app variable
 app = Flask(__name__)
+app.run(debug=True)
 
 
 # define an endpoint which accepts POST requests, and is reachable from the /record endpoint
@@ -41,4 +42,32 @@ def record_engine_temperature():
 # define an endpoint which accepts POST requests, and is reachable from the /collect endpoint
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return {"success": True}, 200
+    # open up a connection to the redis database
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
+    # retrieve all stored engine temperature readings
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list is now retrieved from redis: {engine_temperature_values}")
+
+    if not engine_temperature_values:
+        # case where there are no values in the database
+        logger.warning("No engine temperature data available in the redis database.")
+        return {
+                   "current_engine_temperature": None,
+                   "average_engine_temperature": None,
+                   "message": "No data available"
+               }, 200
+
+    # get the most recent temperature value
+    current_engine_temperature = float(engine_temperature_values[0])
+    logger.info(f"current engine temperature: {current_engine_temperature}")
+
+    # get the mean engine temperature value
+    average_engine_temperature = sum(map(float, engine_temperature_values)) / len(engine_temperature_values)
+    logger.info(f"average engine temperature: {average_engine_temperature}")
+
+    # return the response as a dictionary
+    return {
+               "current_engine_temperature": current_engine_temperature,
+               "average_engine_temperature": average_engine_temperature
+           }, 200
